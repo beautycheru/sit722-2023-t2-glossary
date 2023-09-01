0bb8ecf5548c84f37aae7af26ef3b07bbc7c49ed
beautycheru <beaty.cheru@gmail.com>
2023-09-01 16:28:45 +1000

    DevOps Glossary (Part 4&5)

--- routes/index.js
+++ routes/index.js
@@ -242,6 +242,166 @@ const termsAndDescriptions = [
     references:
       "The DEVSECOPS platform (2023) GitLab. Available at: https://about.gitlab.com/ (Accessed: 30 August 2023). ",
   },
+  {
+    id: 31,
+    term: "Bottleneck(Lean)",
+    description:
+      "A stage in a process that restricts the process's or system's entire capacity (DevOps glossary of terms 2023).",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 20 August 2023). ",
+  },
+  {
+    id: 32,
+    term: "Constraint",
+    description:
+      "A limitation or restriction on a system (DevOps glossary of terms 2023).",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 20 August 2023). ",
+  },
+  {
+    id: 33,
+    term: "DevSecOps",
+    description:
+      "The automation of essential security duties through the incorporation of security controls and processes into the DevOps workflow (DevOps glossary of terms 2023). ",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 20 August 2023). ",
+  },
+  {
+    id: 34,
+    term: "Iterations",
+    description:
+      "A single development cycle, usually lasting one or two weeks (DevOps glossary of terms 2023).",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 20 August 2023). ",
+  },
+  {
+    id: 35,
+    term: "Kanban",
+    description:
+      "A visual approach of activity control that pushes work through a process at a controllable pace. (What is Kanban? explained in 10 minutes: Kanbanize 2023)",
+    references:
+      "What is Kanban? explained in 10 minutes: Kanbanize (2023) Kanban Software for Agile Project Management. Available at: https://kanbanize.com/kanban-resources/getting-started/what-is-kanban (Accessed: 30 August 2023). ",
+  },
+  {
+    id: 36,
+    term: "Black Box testing",
+    description:
+      "A sort of functional testing in which the internal structure, design, and code of software are tested (Plutora, 2021). ",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023).  ",
+  },
+  {
+    id: 37,
+    term: "Open Source",
+    description:
+      "Software that makes its original source code freely available for people to use, share, and modify (Plutora, 2021).",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023). ",
+  },
+  {
+    id: 38,
+    term: "Cloud Computing",
+    description:
+      "Cloud computing is the delivery of compute power, database, storage, applications, and other IT resources via the internet (Plutora, 2021). ",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023).  ",
+  },
+  {
+    id: 39,
+    term: "Cluster",
+    description:
+      "Cloud computing is the delivery of compute power, database, storage, applications, and other IT resources via the internet (Plutora, 2021). ",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023). ",
+  },
+  {
+    id: 40,
+    term: "Everything as a Code",
+    description:
+      "Everything as code treats the whole system as code, which means that everything from bare metal servers to deployment configurations is kept as code in a repository and can be rebuilt or rolled back with the press of a button (Plutora, 2021) ",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023). ",
+  },
+  {
+    id: 41,
+    term: "Fail Fast",
+    description:
+      "is a software development mindset that emphasises early and frequent testing in order to quickly determine whether an idea has value. It is a fundamental component of agile approach (Plutora, 2021. ",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023). ",
+  },
+  {
+    id: 42,
+    term: "Hybrid cloud",
+    description:
+      "A hybrid cloud consists of at least one private and one public cloud, allowing organisations to scale up their on-premises infrastructure to the public cloud to manage any overflow (Plutora, 2021). ",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023).  ",
+  },
+  {
+    id: 43,
+    term: "IaaS",
+    description:
+      "is a type of cloud computing service that provides on-demand computation, storage, and networking resources on a pay-as-you-go basis (Microsoft, 2023). ",
+    references:
+      "What is iaas? infrastructure as a service: Microsoft Azure (2023) Infrastructure as a Service | Microsoft Azure. Available at: https://azure.microsoft.com/en-au/resources/cloud-computing-dictionary/what-is-iaas (Accessed: 31 August 2023)",
+  },
+  {
+    id: 44,
+    term: "Scrum",
+    description:
+      "An Iterative, time-bound, and incremental Agile Framework for completing complex projects (Global Knowledge, 2023).",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 31 August 2023).",
+  },
+  {
+    id: 45,
+    term: "Serverless",
+    description:
+      "A cloud computing execution model in which the cloud provider handles the distribution of machine resources dynamically (Global Knowledge, 2023). ",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 31 August 2023).",
+  },
+  {
+    id: 46,
+    term: "Time to Value",
+    description:
+      "Measure of the time it takes for the business to realize value from a feature (Global Knowledge, 2023).",
+    references:
+      "DevOps glossary of terms (2023) Global Knowledge. Available at: https://www.globalknowledge.com/ca-en/topics/devops/glossary-of-terms/ (Accessed: 31 August 2023).",
+  },
+  {
+    id: 47,
+    term: "Containers",
+    description:
+      "Containers are lightweight packages of your application code together with dependencies such as specific versions of programming language runtimes and libraries required to run your software services (Google Cloud 2022) ",
+    references:
+      "What are containers?  |  google cloud (2022) Google. Available at: https://cloud.google.com/learn/what-are-containers (Accessed: 31 August 2023).  ",
+  },
+  {
+    id: 48,
+    term: "Kubernetes",
+    description:
+      "Kubernetes (sometimes shortened to K8s with the 8 standing for the number of letters between the “K” and the “s”) is an open source system to deploy, scale, and manage containerized applications anywhere (Google Cloud 2022) ",
+    references:
+      "What is Kubernetes? |  google cloud (2022) Google. Available at: https://cloud.google.com/learn/what-is-kubernetes (Accessed: 31 August 2023). ",
+  },
+  {
+    id: 49,
+    term: "Image",
+    description:
+      "An image is a bootable snapshot of a server (in our case, a microservice) including all the code, dependencies, and assets that it needs to run(David,2021).  ",
+    references:
+      "Davis, A. (2021) Bootstrapping microservices with Docker, Kubernetes, and Terraform a project-based guide. Shelter Island, NY: Manning.",
+  },
+  {
+    id: 50,
+    term: "Autonomy",
+    description:
+      "refers to the ability to make decisions independently without having to rely on or subordinate to someone higher up in the hierarchy(Plutora,2021).",
+    references:
+      "DevOps DevOps Terminology: A Glossary (2021) Plutora. Available at: https://www.plutora.com/devops-at-scale/terminology-glossary (Accessed: 31 August 2023). ",
+  },
 ];
 
 /* GET home page. */
